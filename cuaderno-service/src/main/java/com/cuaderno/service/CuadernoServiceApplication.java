fd967662087a968d7cc161c4ed613d6d9d62f56f
BRAMDON <Brandon18092003>
2023-07-25 21:38:12 -0500

    eureka

--- cuaderno-service/src/main/java/com/cuaderno/service/CuadernoServiceApplication.java
+++ cuaderno-service/src/main/java/com/cuaderno/service/CuadernoServiceApplication.java
@@ -2,8 +2,10 @@ package com.cuaderno.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CuadernoServiceApplication {
 
 	public static void main(String[] args) {
